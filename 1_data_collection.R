16fa8c81402e4d84fff842abb388da213f54b4d6
lucelsby <lucelsby@gmail.com>
2022-11-26 13:39:11 +0000

    uploading oil production data

--- 1_data_collection.R
+++ 1_data_collection.R
@@ -19,7 +19,8 @@ pacman::p_load(dplyr,
                readxl,
                lubridate,
                rdbnomics,
-               data.table)
+               data.table,
+               stringr)
 
 
 # clear environment
@@ -57,16 +58,16 @@ mret_ids <- c(
 # define a list of variable names to correspond to each series (must be in order)
 mret_names <- c(
   ## goods and services data
-  "gs_imports",
-  "gs_exports",
-  "gs_imports_cvm",
-  "gs_exports_cvm",
+  "gs_impo",
+  "gs_exp",
+  "gs_imp_cvm",
+  "gs_exp_cvm",
   
   # purely goods data
-  "g_imports",
-  "g_exports",
-  "g_imports_cvm",
-  "g_exports_cvm"
+  "g_imp",
+  "g_exp",
+  "g_imp_cvm",
+  "g_exp_cvm"
 
   )
 
@@ -102,5 +103,71 @@ ons_data_clean <- ons_data_raw %>%
 
 
 
+## Download OECD Data ----------------------------------------------------------
+# use the rdbnomics package to retrieve data from the OECD on members' industrial production
+# this is used in the modelling as a proxy for global demand
 
+# collect time series using rdbnomics
+oecd_industrial_production_raw <- rdb(ids = "OECD/KEI/PRINTO01.OECD.ST.M")
+
+
+# clean
+oecd_industrial_production_clean <- oecd_industrial_production_raw %>%
+  
+  # collect only relevant columns
+  dplyr::select(period, value) %>%
+  
+  # rename
+  dplyr::rename(., ind_prod = value)
+
+
+
+
+## Upload oil production data from input files ---------------------------------
+# two bits of oil-related data are collected from the US Energy Information Administration website
+# global oil production and US oil price
+# these have both been downloaded and stored in the input_data file
+
+# upload from raw csv file
+eia_oil_production_raw <- as.data.frame(t(janitor::clean_names(read.csv("input_data/20221126_eia_oil_production.csv", skip = 1)))) %>%
+  
+  # remove the first column
+  dplyr::select(-1) %>%
+  
+  
+  # remove the empty row
+  dplyr::slice(-c(1:2))
+
+
+# clean
+# create a function to extract the date from the short form in the excel file
+extract_date <- function(x) {
+  
+  # define month numbers to map to names
+  Months <- 1:12
+  names(Months) <- month.abb
+  
+  # if the year number is less than 50, start the date with 2000 otherwise start with 1900
+  ifelse(as.numeric(substr(x, 5, 6)) < 50, 
+         paste0("20", substr(x, 5, 6), "-", 
+                as.character(str_pad(unname(Months[str_to_title(substr(x, 1, 3))]), 2, pad = "0")), "-01"),
+         paste0("19", substr(x, 5, 6), "-", 
+                as.character(str_pad(unname(Months[str_to_title(substr(x, 1, 3))]), 2, pad = "0")), "-01"))
+  
+  
+}
+
+# extract the period from the raw data frame
+period <- extract_date(rownames(eia_oil_production_raw))
+
+# exrtact the time series of global oil production
+oil_prod <- eia_oil_production_raw$V2
+
+# combine into clean data frame
+eia_oil_production_clean <- as.data.frame(cbind(period, oil_prod))
+
+# convert character to date format
+eia_oil_production_clean$period <- as.Date(eia_oil_production_clean$period)
+
+  
 
